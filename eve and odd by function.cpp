fb2ef654ac81da17ba12474775f3cc7c76656260
Brijendra Kumar Yadav <brijendrakrydv@gmail.com>
2021-10-01 11:25:20 +0530

    Slight Improvement

--- eve and odd by function.cpp
+++ eve and odd by function.cpp
@@ -2,17 +2,18 @@
 int check()
 {
 	int a ;
-	printf("enter a number:");
+	printf("Enter a number:");
 	scanf("%d",&a);
 	if(a%2==0)
 	 return 2;
 	 else 
 	 return 0;
 }
- main()
+ void main()
  {
  	int b =check();
- 	printf("%d\n",b);
+ 	// printf("%d\n",b);  
+// We don't need this printf actually because we are printing the number itself so printing 2 is just extra code here
  	if(b==2)
  	printf("Even");
  	else 
