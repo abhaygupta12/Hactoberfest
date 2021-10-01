5815e3228dcac4e8dca6182d65c63dac140ba769
Madven01 <90748462+Madven01@users.noreply.github.com>
2021-10-01 21:37:30 +0530

    swap.cpp
    program to display swapping of two variables using cpp
    #hactoberfest2021

---
+++ swap.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a=5,b=10,temp;
+  cout<<"before swapping:"<<endl;
+  cout<<"a="<<a<<",b="<<b<<endl;
+  temp=a;
+  a=b;
+  b=temp;
+  cout<<"\nafter swapping:"<<endl;
+  cout<<"a="<<a<<",b="<<b<<endl;
+  return0;
+}
+  
